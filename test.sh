ca738c63b5343a5ef62d7ffb84f356a90b139b3a
adam <adam@monkeez.org>
2021-03-08 20:08:38 +0000

    Seriously updated

--- test.sh
+++ test.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-source /home/adam/envs.sh
-echo $home_dir
-echo $git_username
-echo $git_pass
+
+home_dir=$1
+
+source ${home_dir}/envs.sh
+echo ${home_dir}
+echo ${git_username}
+echo ${git_pass}
 echo ${MetaToExclude[@]}
 echo ${MetaNamesToExclude[@]}
+echo ${TargetBranch}
+echo ${SourceBranch}
+echo ${ValidateOnly}
+echo ${pr_number}
+echo ${conf_account}
+echo ${PackageVersion}
+echo ${conf_pat}
+echo ${conf_url}
+echo ${conf_page_id}
+echo ${conf_page_title}
+echo ${deploy_all}
